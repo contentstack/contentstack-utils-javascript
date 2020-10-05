5925808bd9536ec58279060b0731383f54964240
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-10-05 17:02:20 +0530

    Embedded attributes model update

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,16 +1,16 @@
 import { elementToJson } from '../helper/html-to-json';
-import { EmbedTagModel } from '../Models/embeddec-object-model';
+import { EmbedAttributes } from '../Models/embeddec-object-model';
 import { parse } from 'node-html-parser';
 const frameflag = 'documentfragmentcontainer';
 
 declare global {
   interface String {
-    forEachEmbeddedObject(callbackfn: (embededObjectTag: string, object: EmbedTagModel) => void): void;
+    forEachEmbeddedObject(callbackfn: (embededObjectTag: string, object: EmbedAttributes) => void): void;
   }
 }
 
 String.prototype.forEachEmbeddedObject = function (
-  callbackfn: (embededObjectTag: string, object: EmbedTagModel) => void,
+  callbackfn: (embededObjectTag: string, object: EmbedAttributes) => void,
 ): void {
 
   const str = `<${frameflag}>${this.toString()}</${frameflag}>`;
@@ -18,10 +18,10 @@ String.prototype.forEachEmbeddedObject = function (
   const embeddedEntries = root.querySelectorAll(".embedded-entry")
   
   embeddedEntries.forEach((element) => {    
-    callbackfn(element.outerHTML, elementToJson(element) as EmbedTagModel)
+    callbackfn(element.outerHTML, elementToJson(element) as EmbedAttributes)
   })
   const embeddedAsset = root.querySelectorAll(".embedded-asset")
   embeddedAsset.forEach((element) => {
-    callbackfn(element.outerHTML, elementToJson(element) as EmbedTagModel)
+    callbackfn(element.outerHTML, elementToJson(element) as EmbedAttributes)
   })
 };
