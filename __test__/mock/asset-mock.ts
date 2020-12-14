ec1584fb182cb7dccf090e144021d234b7e1c037
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-12-14 12:29:32 +0530

    asset mock change added key _content_type_uid

--- __test__/mock/asset-mock.ts
+++ __test__/mock/asset-mock.ts
@@ -1,30 +1,36 @@
 export const assetContentBlank = {
-    uid: "uid"
+    uid: "uid",
+    _content_type_uid: 'asset'
 }
 
 export const assetContentUrl = {
     uid: "uid",
-    url: "url"
+    url: "url",
+    _content_type_uid: 'asset'
 }
 
 export const assetContentonlyFileName = {
     uid: "uid",
-    filename: "filename"
+    filename: "filename",
+    _content_type_uid: 'asset'
 }
 
 export const assetContentonlyFileNameAndURL = {
     uid: "uid",
     filename: "filename",
-    url: "url"
+    url: "url",
+    _content_type_uid: 'asset'
 }
 
 export const assetContentonlyTitle = {
     uid: "uid",
     title: "title",
+    _content_type_uid: 'asset'
 }
 
 export const assetContentonlyTitleAndUrl = {
     uid: "uid",
     title: "title",
-    url: "url"
+    url: "url",
+    _content_type_uid: 'asset'
 }
