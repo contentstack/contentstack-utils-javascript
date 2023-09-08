3f88d2a9a5e18e98a5397037e1bd7787e7d201d8
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-09-08 14:59:41 +0530

    feat: support for multiple field idx

--- src/entry-editable.ts
+++ src/entry-editable.ts
@@ -8,30 +8,89 @@ export function addTags(entry: EntryModel, contentTypeUid: string, tagsAsObject:
 function getTag(content: object, prefix: string, tagsAsObject: boolean, locale: string): object {
     const tags: any = {}
     Object.entries(content).forEach(([key, value]) => {      
+        if (key === '$') return
+
         switch (typeof value) {
             case "object":
                 if (Array.isArray(value)) {
                     value.forEach((obj, index) => {
+                        const childKey = `${key}__${index}`
+                        /**
+                         * Indexes of array are handled here
+                         * {
+                         *  "array": ["hello", "world"],
+                         *  "$": {
+                         *      "array": {"data-cslp": "content_type_uid.entry_uid.locale.array"}
+                         *      "array__0": {"data-cslp": "content_type_uid.entry_uid.locale.array.0"}
+                         *      "array__1": {"data-cslp": "content_type_uid.entry_uid.locale.array.1"}
+                         *  }
+                         * }
+                         */
+                        tags[childKey] = getTagsValue(`${prefix}.${key}.${index}`, tagsAsObject)
                         if (typeof obj !== 'undefined' && obj !== null && obj._content_type_uid !== undefined && obj.uid !== undefined) {
+                            /**
+                             * References are handled here
+                             * {
+                             *  "reference": [{
+                             *      "title": "title",
+                             *      "uid": "ref_uid",
+                             *      "_content_type_uid": "ref_content_type_uid",
+                             *     "$": {"title": {"data-cslp": "ref_content_type_uid.ref_uid.locale.title"}}
+                             *  }]
+                             * }
+                             */
                             value[index].$ = getTag(obj, `${obj._content_type_uid}.${obj.uid}.${obj.locale || locale}`, tagsAsObject, locale)
-                        }else {
-                            if (typeof obj === "object") {
-                                obj.$ = getTag(obj, `${prefix}.${key}.${index}`, tagsAsObject, locale)
-                                tags[key] = getTagsValue(`${prefix}.${key}.${index}`, tagsAsObject)
-
-                            } else {
-                                tags[key] = getTagsValue(`${prefix}.${key}`, tagsAsObject)
-                            }
+                        }else if (typeof obj === "object") {
+                            /**
+                             * Objects inside Array like modular blocks are handled here
+                             * {
+                             *  "array": [{
+                             *    "title": "title",
+                             *    "$": {"title": {"data-cslp": "content_type_uid.entry_uid.locale.array.0.title"}}
+                             *  }]
+                             * }
+                             */
+                            obj.$ = getTag(obj,`${prefix}.${key}.${index}`, tagsAsObject, locale)
                         }
                     })
                 }else {
                     if (value) {
+                        /**
+                         * Objects are handled here
+                         * {
+                         *  "object": {
+                         *      "title": "title",
+                         *      "$": {
+                         *          "title": {"data-cslp": "content_type_uid.entry_uid.locale.object.title"}
+                         *      }
+                         *  },
+                         * }
+                         */
                         value.$ = getTag(value, `${prefix}.${key}`, tagsAsObject, locale)
                     }
                 }
+                /**
+                 * {
+                 *  "object": {
+                 *      "title": "title",
+                 *  },
+                 *  "array": ["hello", "world"]
+                 *  "$": {
+                 *      "object": {"data-cslp": "content_type_uid.entry_uid.locale.object"},
+                 *      "array": {"data-cslp": "content_type_uid.entry_uid.locale.array"}
+                 *  }
+                 * }
+                 */
                 tags[key] = getTagsValue(`${prefix}.${key}`, tagsAsObject)
                 break;
             default:
+                /**
+                 * All primitive values are handled here
+                 * {
+                 *  "title": "title",
+                 *  "$": {title: {"data-cslp": "content_type_uid.entry_uid.locale.title"}}
+                 * }
+                 */
                 tags[key] = getTagsValue(`${prefix}.${key}`, tagsAsObject)
         }
     })
