9a0027e38a7f896a3c661ecc9c4c75947dc5968b
Nadeem Patwekar <nadeem.patwekar@contentstack.com>
2024-05-30 14:55:25 +0530

    chore: adds optional chaining

--- src/updateAssetURLForGQL.ts
+++ src/updateAssetURLForGQL.ts
@@ -1,19 +1,19 @@
 export function updateAssetURLForGQL(gqlResponse:any) {
   try {
-    const response = gqlResponse.data;
+    const response = gqlResponse?.data;
     for (let contentType in response) {
       const entry = response[contentType]; // page_json_rte
-      for (let key in entry) {
-        const field = entry[key];
-        if (field && field.embedded_itemsConnection) { // rte field with embedded items e.g rte_2
-          const edges = field.embedded_itemsConnection.edges;
+      for (let field in entry) {
+        const fieldData = entry[field];
+        if (fieldData && fieldData.embedded_itemsConnection) { // rte field with embedded items e.g rte_2
+          const edges = fieldData?.embedded_itemsConnection?.edges;
           edges.forEach((edge:any) => {
             const node = edge.node;
-            if (node.url && node.filename) {
+            if (node?.url && node?.filename) {
               
               if (!node?.system?.uid) throw new Error('Asset UID not found in the response');
 
-              const correspondingAsset = field.json.children.find((child:any) => child.attrs['asset-uid'] === node.system.uid);
+              const correspondingAsset = fieldData?.json?.children?.find((child:any) => child.attrs['asset-uid'] === node.system.uid);
               correspondingAsset.attrs['asset-link'] = node.url;
             }
           });
